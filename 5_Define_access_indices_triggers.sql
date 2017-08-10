1fde9e72acdefcad36c501c4fa90e157ee34ac4f
Joanna <j.daniow@gmail.com>
2017-08-10 14:22:17 +0200

    Last refactor

--- 5_Define_access_indices_triggers.sql
+++ 5_Define_access_indices_triggers.sql
@@ -1,7 +1,6 @@
 USE car_rental_company;
 
 -- zadanie 5
-
 CREATE USER 'user_ra'@'localhost' IDENTIFIED BY '1234';
 
 CREATE USER 'user_ro'@'localhost' IDENTIFIED BY '1234';
@@ -11,16 +10,15 @@ CREATE USER 'user_rw_projekt'@'localhost' IDENTIFIED BY '1234';
 -- 5 a
 GRANT SELECT ON car_rental_company.* TO 'user_ro'@'localhost';
 
--- 5 b odpowiednik GRANT SELECT, INSERT, UPDATE, DELETE ON car_rental_compay.car TO 'user_rw_projekt'@'localhost';
-GRANT ALL ON car_rental_compay.car TO 'user_rw_projekt'@'localhost';
+-- 5 b odpowiednik GRANT SELECT, INSERT, UPDATE, DELETE ON car_rental_company.car TO 'user_rw_projekt'@'localhost';
+GRANT ALL ON car_rental_company.car TO 'user_rw_projekt'@'localhost';
 
 REVOKE CREATE, DROP, REFERENCES, INDEX, ALTER, CREATE VIEW, SHOW VIEW, TRIGGER 
- ON car_rental_compay.car FROM 'user_rw_projekt'@'localhost';
+ ON car_rental_company.car FROM 'user_rw_projekt'@'localhost';
 
-FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'user_rw_projekt'@'localhost';
 
 -- zadanie 6
-
 CREATE INDEX full_name ON customer (first_name, surname);
  -- these two are often used to search for the user, e.g. every time customers walks in or phones employee has to find his "profile"
 
@@ -31,7 +29,6 @@ CREATE INDEX user_rentals ON rental_transaction (car_id, customer_id);
  -- these are the two most searched fields, putting them in one index eliminates the need to access the rest of the tablen
 
 -- zadanie 7
-
 CREATE TRIGGER min_amount BEFORE INSERT ON rental_transaction
        FOR EACH ROW 
        SET NEW.amount_in_PLN = IF(NEW.amount_in_PLN < 100, 100, NEW.amount_in_PLN);
